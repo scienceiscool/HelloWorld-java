0ad6b448858ca082f591b87afa36ad9fe03ab460
Kathy <kathysaad@gmail.com>
2016-03-16 20:28:14 -0700

    Hello World in Java

---
+++ Packages/com/example/java/Main.java
@@ -0,0 +1,11 @@
+package com.example.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// println = print line
+		System.out.println("Hello from Java!");
+		System.out.println(args[0]);
+	}
+
+}
